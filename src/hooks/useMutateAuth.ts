92d199b1563ae002fccb4832afc729356caa0b5f
jonghoon.lee <greatest86@gmail.com>
2026-05-20 14:50:42 +0900

    CSRF 토큰을 가져오는 useEffect 추가 및 타입 정의 수정

--- src/hooks/useMutateAuth.ts
+++ src/hooks/useMutateAuth.ts
@@ -1,14 +1,26 @@
 import axios from 'axios'
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useMutation } from '@tanstack/react-query'
 import useStore from '../store'
-import { Credential } from '../types'
+import { Credential, CsrfToken } from '../types'
 import { useError } from '../hooks/useError'
 
 export const useMutateAuth = () => {
   const navigate = useNavigate()
   const resetEditedTask = useStore((state) => state.resetEditedTask)
   const { switchErrorHandling } = useError()
+
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get<CsrfToken>(
+        `${process.env.REACT_APP_API_URL}/csrf`,
+      )
+      axios.defaults.headers.common['X-CSRF-TOKEN'] = data.csrf_token
+    }
+    getCsrfToken()
+  }, [])
+
   const loginMutation = useMutation(
     async (user: Credential) =>
       await axios.post(`${process.env.REACT_APP_API_URL}/login`, user),
@@ -23,7 +35,7 @@ export const useMutateAuth = () => {
           switchErrorHandling(err.response.data)
         }
       },
-    }
+    },
   )
   const registerMutation = useMutation(
     async (user: Credential) =>
@@ -36,7 +48,7 @@ export const useMutateAuth = () => {
           switchErrorHandling(err.response.data)
         }
       },
-    }
+    },
   )
   const logoutMutation = useMutation(
     async () => await axios.post(`${process.env.REACT_APP_API_URL}/logout`),
@@ -52,7 +64,7 @@ export const useMutateAuth = () => {
           switchErrorHandling(err.response.data)
         }
       },
-    }
+    },
   )
   return { loginMutation, registerMutation, logoutMutation }
 }
